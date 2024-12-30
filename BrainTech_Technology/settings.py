5ae4d7271c90d768e511925cfac897cf6409d456
Vishnu Kumar Ghosh <120448653+ghosh-vishnu@users.noreply.github.com>
2024-12-30 17:12:01 +0530

    Update settings.py

--- BrainTech_Technology/settings.py
+++ BrainTech_Technology/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'BrainTech_App',  # Ensure this name matches your app directory
+    'mod.wsgi_server',
 ]
 
 MIDDLEWARE = [
